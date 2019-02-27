c673be18e54a8cbfd86662c1efb253237dc70f38
Alec <alec.goncalves@student.shu.edu>
2019-02-26 21:58:38 -0500

    query 8, 9, 10 still incomplete

--- Goncalves-Alec-Assignment2.sql
+++ Goncalves-Alec-Assignment2.sql
@@ -9,8 +9,8 @@ sum(P.amount) as "Total Spent"
 from Customer C
 	join payment as P 
 		on C.customer_id = P.customer_id
-group by customer_id
-order by C.last_name, C.first_name;
+group by C.customer_id
+order by C.last_name, sum(P.amount);
 
 /**
  *Question 2
@@ -28,7 +28,7 @@ where A.postal_code is null
  *Question 3
  */
  select
- title
+	title
  from film
  where title like '%FIRE%'
 	or title like '%DOCTOR%';
@@ -45,7 +45,7 @@ from actor A
 	join film_actor FA
 		on A.actor_id = FA.actor_id
 group by actor_id
-order by A.last_name, A.first_name;
+order by A.last_name, count(FA.film_id);
 
 /**
  *Question 5
@@ -64,9 +64,59 @@ order by avg(f.length);
 /**
  *Question 6
  */
+select
+	S.store_id,
+	sum(P.amount) as "Total Amount"
+from store as S
+join customer as C
+	on S.store_id = C.store_id
+join payment as P
+	on P.customer_id = C.customer_id
+group by S.store_id
+order by sum(P.amount) desc;
 
-        
+/**
+ *Question 7
+ */
+select
+	C.first_name,
+    C.last_name,
+    C.email,
+    sum(P.amount) as "Total spent"
+from customer as C
+join store as S
+	on S.store_id = C.store_id
+join address as A
+	on S.address_id = A.address_id
+join city as CI
+	on A.city_id = CI.city_id
+join country as CO
+	on CI.country_id = CO.country_id
+join payment as P
+	on P.customer_id = C.customer_id
+where CO.country = "Canada"
+group by C.customer_id
+order by C.last_name;
 
+/**
+ *Question 8
+ */
+start transaction;
+
+insert into 
+rental(rental_date, inventory_id, customer_id, staff_id)
+	values(
+		now(),
+		(select min(inventory_id) from Inventory I
+			join film F
+				on I.film_id = F.film_id
+			where F.title = "Hunger Roof" and I.store_id = 2),
+        (select customer_id from customer
+			where first_name = "Mathew" and last_name = "Bolin"),
+        (select staff_id from staff
+			where first_name = "Jon" and last_name = "Stephens" and store_id = 2)
+            );
+Rollback;
 
 
 
